7789acfa15539f88481b9e7f4fe5cc36d6ed7bc5
ltthuyth99 <30860051+ltthuyth99@users.noreply.github.com>
2017-08-10 08:44:05 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
-app.post('/echo', function(req, res) {
+app.post('/insurancebot', function(req, res) {
     //var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
     return res.json({
         msg:'hello'
