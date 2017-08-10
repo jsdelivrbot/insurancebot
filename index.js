a3c0225cda53b764c3bc2e363e07a41dbe604944
ltthuyth99 <30860051+ltthuyth99@users.noreply.github.com>
2017-08-10 10:44:55 +0700

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,17 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/webhook', function(request, response) {
-  response.render('pages/index');
+  console.log(request.body);
+  if (request.body.object === 'page') {
+    request.body.entry.forEach((entry) => {
+      entry.messaging.forEach((event) => {
+        if (event.message && event.message.text) {
+          sendMessage(event);
+        }
+      });
+    });
+    response.status(200).end();
+  }
 });
 
 app.post('/webhook', function(req, res) {
@@ -18,6 +28,50 @@ app.post('/webhook', function(req, res) {
     return res.json({
         msg:'hello'
     });
+});
+
+function sendMessage(event) {
+  let sender = event.sender.id;
+  let text = event.message.text;
+
+  let apiai = apiaiApp.textRequest(text, {
+    sessionId: 'tabby_cat'
+  });
+
+  apiai.on('response', (response) => {
+    console.log(response)
+    let aiText = response.result.fulfillment.speech;
+
+    request({
+      url: 'https://graph.facebook.com/v2.6/me/messages',
+      qs: {access_token: PAGE_ACCESS_TOKEN},
+      method: 'POST',
+      json: {
+        recipient: {id: sender},
+        message: {text: aiText}
+      }
+    }, (error, response) => {
+      if (error) {
+          console.log('Error sending message: ', error);
+      } else if (response.body.error) {
+          console.log('Error: ', response.body.error);
+      }
+    });
+  });
+
+  apiai.on('error', (error) => {
+    console.log(error);
+  });
+
+  apiai.end();
+}
+
+/* Webhook for API.ai to get response from the 3rd party API */
+app.post('/ai', function (req, res) {
+  if (req.body.result.action === 'weather') {
+    console.log('*** weather ***');
+    let city = req.body.result.parameters['geo-city'];
+  }
 });
 
 app.listen(app.get('port'), function() {
